7507600226af19b030b57666fbfc8b4563523154
wb-mwb403042@alibaba-inc.com <wb-mwb403042@alibaba-inc.com>
2021-06-06 22:39:44 +0800

    chore: 添加ts类型定义

---
+++ src/utils/index.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: number) => (value === 0 ? false : !value);
+
+// 在一个函数里，改变传入的对象本身是不好的
+export const cleanObject = (object: any) => {
+  // Object.assign({}, object)
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    // 0
+    const value = result[key];
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// export const debounce = (func: any, delay?: number) => {
+//   let timeout;
+//   return () => {
+//     if (timeout) {
+//       clearTimeout(timeout);
+//     } else {
+//       timeout = setTimeout(() => {
+//         func();
+//       }, delay);
+//     }
+//   }
+// }
+
+export const useDebounce = (value: any, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
