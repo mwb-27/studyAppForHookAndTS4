e20c2479fdbaca30c8d633ec261ea63c1cacd609
wb-mwb403042@alibaba-inc.com <wb-mwb403042@alibaba-inc.com>
2021-06-21 18:24:09 +0800

    feat: 登录注册loading和错误处理

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useAuth } from "context/auth-context";
 import { Form, Input } from "antd";
 import { LongButton } from "unauthenticated-app/index";
+import { useAsync } from "utils/use-async";
 
 // interface Base {
 //   id: number
@@ -17,14 +18,25 @@ import { LongButton } from "unauthenticated-app/index";
 // // 鸭子类型(duck typing)：面向接口编程 而不是 面向对象编程
 // const a = {id: 1, name: 'jack'}
 // test(a)
-const apiUrl = process.env.REACT_APP_API_URL;
 
-export const LoginScreen = () => {
-  const { login, user } = useAuth();
+export const LoginScreen = ({
+  onError,
+}: {
+  onError: (error: Error) => void;
+}) => {
+  const { login } = useAuth();
+  const { run, isLoading } = useAsync(undefined, { throwOnError: true });
 
   // HTMLFormElement extends Element
-  const handleSubmit = (values: { username: string; password: string }) => {
-    login(values);
+  const handleSubmit = async (values: {
+    username: string;
+    password: string;
+  }) => {
+    try {
+      await run(login(values));
+    } catch (e) {
+      onError(e);
+    }
   };
 
   return (
@@ -42,7 +54,7 @@ export const LoginScreen = () => {
         <Input placeholder={"密码"} type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        <LongButton htmlType={"submit"} type={"primary"}>
+        <LongButton loading={isLoading} htmlType={"submit"} type={"primary"}>
           登录
         </LongButton>
       </Form.Item>
