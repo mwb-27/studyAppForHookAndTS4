b9d9c4cbcdbba04917bbe7874c14f32234106eeb
wb-mwb403042@alibaba-inc.com <wb-mwb403042@alibaba-inc.com>
2021-06-21 14:07:31 +0800

    fix: 去除警告信息

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,26 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value: number) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
+export const isVoid = (value: unknown) =>
+  value === undefined || value === null || value === "";
+
+// let a: object
+// a = {name: 'jack'}
+// a = () => {
+// }
+// a = new RegExp('')
+//
+// let b: { [key: string]: unknown }
+// b = {name: 'Jack'}
+// b = () => {}
 // 在一个函数里，改变传入的对象本身是不好的
-export const cleanObject = (object: object) => {
+export const cleanObject = (object: { [key: string]: unknown }) => {
   // Object.assign({}, object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
-    // @ts-ignore
     const value = result[key];
-    if (isFalsy(value)) {
-      // @ts-ignore
+    if (isVoid(value)) {
       delete result[key];
     }
   });
@@ -20,23 +30,39 @@ export const cleanObject = (object: object) => {
 export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
+    // TODO 依赖项里加上callback会造成无限循环，这个和useCallback以及useMemo有关系
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
 
-// export const debounce = (func: any, delay?: number) => {
+// const debounce = (func, delay) => {
 //   let timeout;
-//   return () => {
+//   return (...param) => {
 //     if (timeout) {
 //       clearTimeout(timeout);
-//     } else {
-//       timeout = setTimeout(() => {
-//         func();
-//       }, delay);
 //     }
+//     timeout = setTimeout(function() {
+//       func(...param);
+//     }, delay);
 //   }
 // }
+// const log = debounce(() => console.log('call'), 5000)
+// log()
+// log()
+// log()
+//   ...5s
+// 执行！
 
-export const useDebounce = <T>(value: T, delay: number): T => {
+// debounce 原理讲解：
+// 0s ---------> 1s ---------> 2s --------> ...
+//     一定要理解：这三个函数都是同步操作，所以它们都是在 0~1s 这个时间段内瞬间完成的；
+//     log()#1 // timeout#1
+//     log()#2 // 发现 timeout#1！取消之，然后设置timeout#2
+//     log()#3 // 发现 timeout#2! 取消之，然后设置timeout#3
+//             // 所以，log()#3 结束后，就只剩timeout#3在独自等待了
+
+// 后面用泛型来规范类型
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
@@ -49,27 +75,17 @@ export const useDebounce = <T>(value: T, delay: number): T => {
   return debouncedValue;
 };
 
-export const useArray = <T>(param: T[]) => {
-  // hello，请把作业写在这里吧，写完记得再对照作业要求检查一下
-  const [value, setValue] = useState(param);
-  const removeIndex = (index: number) => {
-    const copy = [...value];
-    copy.splice(index, 1);
-    setValue(copy);
-  };
-  const clear = () => {
-    setValue([]);
-  };
-  const add = (person: T) => {
-    const newState = [...value, person];
-    setValue(newState);
-  };
-
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
   return {
     value,
     setValue,
-    clear,
-    removeIndex,
-    add,
+    add: (item: T) => setValue([...value, item]),
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
   };
 };
