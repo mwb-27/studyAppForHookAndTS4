ae330bb5ca041800b7a97ba338fdbe5ad8c31216
mwb-27 <545879066@qq.com>
2021-07-01 15:52:01 +0800

    chore: chore: 用 redux-toolkit 替换context管理auth, 自写有问题

---
+++ src/store/auth.slice.ts
@@ -0,0 +1,34 @@
+import { User } from "screens/project-list/search-panel";
+import { createSlice } from "@reduxjs/toolkit";
+import * as auth from "auth-provider";
+import { AuthForm, bootstrapUser } from "context/auth-context";
+import { AppDispatch, RootState } from "store";
+
+interface State {
+  user: User | null;
+}
+
+const initialState: State = {
+  user: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser(state, action) {},
+  },
+});
+
+const { setUser } = authSlice.actions;
+
+export const selectUser = (state: RootState) => state.auth.user;
+
+export const login = (form: AuthForm) => (dispatch: AppDispatch) =>
+  auth.login(form).then((user) => dispatch(setUser(user)));
+export const register = (form: AuthForm) => (dispatch: AppDispatch) =>
+  auth.register(form).then((user) => dispatch(setUser(user)));
+export const logout = () => (dispatch: AppDispatch) =>
+  auth.logout().then(() => setUser(null));
+export const bootstrap = () => (dispatch: AppDispatch) =>
+  bootstrapUser().then((user) => dispatch(setUser(user)));
